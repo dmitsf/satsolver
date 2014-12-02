a2fecd6e911c8279cb4add0a63dc23f9f834cddf
Dmitry Frolov <dmitsf@gmail.com>
2014-12-02 21:41:48 +0400

    modified:   gen.py

--- gen.py
+++ gen.py
@@ -1,31 +1,46 @@
 # http://www.boolsat.com/
 
 from itertools import product
+from math import sqrt
+s = lambda x: str(x)
 
 
-def generate(N):
+def generate(N, with_sectors=False, q=None):
+
 	numbs = xrange(1, N+1)
 	st = ''
 
 	for x, a, v, b in product(numbs, numbs, numbs, numbs):
 		if a > b: 
-			st += ' '.join(['-'+str(x*100+a*10+v), '-'+str(x*100+b*10+v), '0']) + ' '
+			st += ' '.join(['-'+s(x*100+a*10+v), '-'+s(x*100+b*10+v), '0']) + ' '
 
 	for x, a, v, b in product(numbs, numbs, numbs, numbs):
 		if a > b:
-			st += ' '.join(['-'+str(a*100+x*10+v), '-'+str(b*100 + x*10+v), '0']) + ' '
+			st += ' '.join(['-'+s(a*100+x*10+v), '-'+s(b*100 + x*10+v), '0']) + ' '
 
 	for x, y, v, u in product(numbs, numbs, numbs, numbs):
 		if v > u:
-			st += ' '.join(['-'+str(100*x+10*y+v), '-'+str(100*x+10*y+u), '0']) + ' '
+			st += ' '.join(['-'+s(100*x+10*y+v), '-'+s(100*x+10*y+u), '0']) + ' '
 
 	for x in numbs:
 		for y in numbs:
 			for n in numbs:
-				st += str(100*x+10*y+n) + ' '
+				st += s(100*x+10*y+n) + ' '
 			st += "0 "
 
-	st += ' 111 0 222 0 333 0'
+	k = 0
+	if with_sectors:
+		for x, y, u, v, n in product(numbs, numbs, numbs, numbs, numbs):
+			#print x//q == True, x, q
+			if x!=u and y!=v and ((x-1)//q == (u-1)//q) and (((y-1)//q) == ((v-1)//q)):
+				print x, y, "|", u, v, n
+
+				k += 1
+				st += ' '.join(['-'+s(x*100+y*10+n), '-'+s(u*100 + v*10+n), '0']) + ' '
+
+	print k
+
+	st += ' 111 0 222 0 333 444 0'
 	return st
 
 
@@ -35,6 +50,7 @@ def save(strs, filename='default.txt'):
 		f.write(strs)
 		f.close()
 
+
 if __name__ == '__main__':
 
 	try: 
@@ -42,7 +58,11 @@ if __name__ == '__main__':
 	except:
 		N = 3
 
-	sq = generate(N)
+	if N in [4, 9]:
+		sq = generate(N, with_sectors=True, q=sqrt(N))
+	else:
+		sq = generate(N)
+	
 	save(sq)
 
 

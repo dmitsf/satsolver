ad2418e2b8c64e70ee4040c1f0fd658e6c3c8bd3
dmitsf <dmitsf@gmail.com>
2017-03-05 15:02:49 +0300

    Update gen.py

--- gen.py
+++ gen.py
@@ -10,15 +10,15 @@ def generate(N, with_sectors=False, q=None, init=''):
 	numbs = xrange(1, N+1)
 	st = ''
 
-	for x, a, v, b in product(numbs, numbs, numbs, numbs):
+	for x, a, v, b in product(numbs, repeat=4):
 		if a > b: 
 			st += ' '.join(['-'+s(x*100+a*10+v), '-'+s(x*100+b*10+v), '0']) + ' '
 
-	for x, a, v, b in product(numbs, numbs, numbs, numbs):
+	for x, a, v, b in product(numbs, repeat=4):
 		if a > b:
 			st += ' '.join(['-'+s(a*100+x*10+v), '-'+s(b*100 + x*10+v), '0']) + ' '
 
-	for x, y, v, u in product(numbs, numbs, numbs, numbs):
+	for x, y, v, u in product(numbs, repeat=4):
 		if v > u:
 			st += ' '.join(['-'+s(100*x+10*y+v), '-'+s(100*x+10*y+u), '0']) + ' '
 
@@ -29,7 +29,7 @@ def generate(N, with_sectors=False, q=None, init=''):
 			st += "0 "
 
 	if with_sectors:
-		for x, y, u, v, n in product(numbs, numbs, numbs, numbs, numbs):
+		for x, y, u, v, n in product(numbs, repeat=5):
 			if (x>u or y>v) and ((x-1)//q == (u-1)//q) and (((y-1)//q) == ((v-1)//q)):
 				st += ' '.join(['-'+s(x*100+y*10+n), '-'+s(u*100 + v*10+n), '0']) + ' '
 
